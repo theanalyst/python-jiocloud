b82b653a5cd1b8a608125923bdd4a81547074831
Dan Bode <bodepd@gmail.com>
2014-08-29 13:04:39 +0000

    Update apply resources to allow deletion
    Now it can delete based on project id

--- jiocloud/apply_resources.py
+++ jiocloud/apply_resources.py
@@ -28,14 +28,16 @@ def read_resources(path):
     fp = file(path)
     return yaml.load(fp)['resources']
 
-def get_existing_servers(nova_client, project_tag=None):
+def get_existing_servers(nova_client, project_tag=None, attr_name='name'):
     """
     This method accepts an option project tag
     """
+    # NOTE we should check for servers only in a certain state
     servers = nova_client.servers.list()
     if project_tag:
         servers = [elem for elem in servers if elem.name.endswith('_' + project_tag) ]
-    return [s.name for s in servers]
+    return [getattr(s, attr_name) for s in servers]
+
 
 def generate_desired_servers(resources, project_tag=None):
     """
@@ -86,13 +88,31 @@ def create_server(nova_client, name, flavor, image, networks, **keys):
         status = instance.status
         print "status: %s" % status
 
+def delete_servers(nova_client, project_tag):
+    servers = get_existing_servers(nova_client, project_tag=project_tag, attr_name='id')
+    for uuid in servers:
+        print "Deleting uuid: %s"%(uuid)
+        nova_client.servers.delete(uuid)
+
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('resource_file_path', help='Path to resource file')
-    argparser.add_argument('--project_tag', help='Project tag')
+    subparsers = argparser.add_subparsers(dest='action', help='Action to perform')
+
+    apply_parser  = subparsers.add_parser('apply', help='Apply a resource file')
+    apply_parser.add_argument('resource_file_path', help='Path to resource file')
+    apply_parser.add_argument('--project_tag', help='Project tag')
+
+    delete_parser = subparsers.add_parser('delete', help='Delete a project')
+    delete_parser.add_argument('project_tag', help='Id of project to delete')
+
     args = argparser.parse_args()
     nova_client = get_nova_client()
-    servers = servers_to_create(get_nova_client(),
-                                args.resource_file_path,
-                                project_tag=args.project_tag)
-    create_servers(nova_client, servers)
+    if args.action == 'apply':
+        servers = servers_to_create(get_nova_client(),
+                                    args.resource_file_path,
+                                    project_tag=args.project_tag)
+        create_servers(nova_client, servers)
+    elif args.action == 'delete':
+        if not args.project_tag:
+            argparser.error("Must set project tag when action is delete")
+        delete_servers(nova_client, project_tag=args.project_tag)
