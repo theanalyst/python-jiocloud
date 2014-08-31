c6355d8aae074036482d9b3d7e3b46e145d4ffe6
Soren Hansen <Soren.Hansen@ril.com>
2014-08-31 13:23:49 +0530

    Fix syntax error
    This explains why HTTPErrors weren't being caught properly..

--- jiocloud/orchestrate.py
+++ jiocloud/orchestrate.py
@@ -38,7 +38,7 @@ class DeploymentOrchestrator(object):
     def ping(self):
         try:
             return bool(self.etcd.machines)
-        except etcd.EtcdError, etcd.EtcdException, HTTPError:
+        except (etcd.EtcdError, etcd.EtcdException, HTTPError) as e:
             return False
 
     def update_own_info(self, hostname, interval=60, version=None):
