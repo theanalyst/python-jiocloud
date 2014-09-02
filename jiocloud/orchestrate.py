a97581160d8c45123531d13fc08542c79066750c
Soren Hansen <Soren.Hansen@ril.com>
2014-09-02 16:56:28 +0530

    etcd.EtcdError disappeared

--- jiocloud/orchestrate.py
+++ jiocloud/orchestrate.py
@@ -81,7 +81,7 @@ class DeploymentOrchestrator(object):
     def ping(self):
         try:
             return bool(self.etcd.machines)
-        except (etcd.EtcdError, etcd.EtcdException, HTTPError) as e:
+        except (etcd.EtcdException, HTTPError) as e:
             return False
 
     def update_own_info(self, hostname, interval=60, version=None):
